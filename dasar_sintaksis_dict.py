5d79f86360f974c89eb0641ff125a4d5b87f7b8e
theodores <theodres.indra@ultrasakti.com>
2022-01-23 07:27:44 +0700

    Dict and JSON

--- dasar_sintaksis_dict.py
+++ dasar_sintaksis_dict.py
@@ -4,18 +4,26 @@ users = {
     "username": "Bret",
     "email": "Sincere@april.biz",
     "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496",
-      }
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496",
+        }
     }
 }
 
 print(users)
 print(users["address"]["geo"]["lat"])
 
+print('\n"Ubah dict to json')
+import json
 
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open('result.json', 'w') as file:
+    json.dump(users, file)
